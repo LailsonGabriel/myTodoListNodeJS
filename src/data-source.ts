5b27577374e67668b86acb1a6568d2f2f10f0ff2
Lailson Gabriel <lailsongabrielturbo@gmail.com>
2024-08-12 00:00:36 -0300

    feat: configurando para subir no render

--- src/data-source.ts
+++ src/data-source.ts
@@ -5,12 +5,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const AppDataSource = new DataSource({
-  type: process.env.DB_TYPE as "mysql" | "mariadb" | "postgres" | "sqlite",
+  type: "postgres",
   host: process.env.DB_HOST || "localhost",
   port: parseInt(process.env.DB_PORT || "3306", 10),
-  username: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "password",
-  database: process.env.DB_NAME || "test",
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME || "todoList",
   entities: [User, Task],
   synchronize: false,
   logging: false,
