a44abc295ca58227f41d9d9121ba6d1fe504c7b2
Lailson Gabriel <lailsongabrielturbo@gmail.com>
2024-08-11 23:47:58 -0300

    feat: adicionado pg ao projeto e configurado .env

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,17 +1,19 @@
-import { DataSource } from "typeorm"
-import { User } from "./entities/user"
-import { Task } from "./entities/task"
+import { DataSource } from "typeorm";
+import { User } from "./entities/user";
+import { Task } from "./entities/task";
+import dotenv from "dotenv";
+dotenv.config();
 
 const AppDataSource = new DataSource({
-    type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "root",
-    password: "Lailson-29",
-    database: "todoList",
-    entities: [User, Task],
-    synchronize: false,
-    logging: false,
+  type: process.env.DB_TYPE as "mysql" | "mariadb" | "postgres" | "sqlite",
+  host: process.env.DB_HOST || "localhost",
+  port: parseInt(process.env.DB_PORT || "3306", 10),
+  username: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "password",
+  database: process.env.DB_NAME || "test",
+  entities: [User, Task],
+  synchronize: false,
+  logging: false,
 });
 
-export default AppDataSource
+export default AppDataSource;
